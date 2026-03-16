35c8d0817eb1665e097bd78c740ac8bfabeeebb1
MUKILTEO\1725934 <leavittjonathan8@gmail.com>
2026-03-16 10:13:10 -0700

    start of item crafting

---
+++ Assets/Jonathan Folder/Scripts/ItemCrafting.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using System;
+using UnityEngine;
+
+public class ItemCrafting : MonoBehaviour
+{
+    public string[] itemA;
+    public string[] itemB;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    static void Main()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDrag()
+    {
+        
+    }
+}
