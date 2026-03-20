923469ae207995d3f7d0a9b8075f4ee63622142d
MUKILTEO\1630107 <1630107@mukilteo.wednet.edu>
2026-03-20 09:46:53 -0700

    Almost done

--- Assets/AbigailCode/Code/Interact.cs
+++ Assets/AbigailCode/Code/Interact.cs
@@ -19,7 +19,18 @@ public class Interact : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(e))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
     }
 
     void StartDialogue()
@@ -34,6 +45,20 @@ public class Interact : MonoBehaviour
         {
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine(index));
+        }
+        else
+        {
+            gameObject.SetActive(false); 
         }
     }
 }
