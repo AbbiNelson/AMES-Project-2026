014401915c5cff82c41f2c9141f83decc3988dfa
MUKILTEO\1725934 <leavittjonathan8@gmail.com>
2026-03-23 09:26:13 -0700

    Posibly finished cant really test due to abigail's code stopping testing

---
+++ Assets/Jonathan Folder/Scripts/DraggableItem.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Image image;
+    [HideInInspector] public Transform parentAfterDrag;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Started dragging " + gameObject.name);
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("Stopped dragging " + gameObject.name);
+        transform.SetParent(parentAfterDrag);
+    }
+
+}
