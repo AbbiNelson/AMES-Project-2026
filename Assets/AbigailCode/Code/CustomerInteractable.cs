e6f9f60e5e7d1a8805a66b493591ae0fe976deaf
MUKILTEO\1630107 <1630107@mukilteo.wednet.edu>
2026-03-24 09:54:14 -0700

    interaction coming along

--- Assets/AbigailCode/Code/CustomerInteractable.cs
+++ Assets/AbigailCode/Code/CustomerInteractable.cs
@@ -4,20 +4,31 @@ using System.Collections.Generic;
 public class CustomerInteractable : MonoBehaviour, IInteractable
 {
     public bool IsTalking {  get; private set; }
-
+    public bool InRange {  get; private set; }
     public string CustomerID {  get; private set; }
+
     public bool CanInteract()
     {
-        throw new System.NotImplementedException();
+        return !IsTalking && InRange;
     }
 
     public void Interact()
     {
-        throw new System.NotImplementedException();
+        if (!CanInteract()) return;
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        CustomerID ??= GlobalHelper.GenerateUniqueID(gameObject);
+    }
+
+    private void CutsomerTalking()
+    {
+        SetTalked(true);
+    }
+
+    public void SetTalked(bool talked)
+    {
+        IsTalking = talked;
     }
 }
