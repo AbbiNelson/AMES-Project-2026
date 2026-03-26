1031b92f68d65e661734066680546927d188921a
MUKILTEO\1726187 <1726187@mukilteo.wednet.edu>
2026-03-26 10:23:37 -0700

    finish credits functionality and began pause menu/pausing

---
+++ Assets/Axel S/Menu Scripts/Credit placeholder.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Creditplaceholder : MonoBehaviour
+{
+        public void placeholder()
+        {
+            Debug.Log("THIS ISNT A THING YET YOU FILTHY FREAK");
+        }
+}
