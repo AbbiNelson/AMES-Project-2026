351546399d254e1da0a9f35394acf85f7fe0742e
MUKILTEO\1725934 <leavittjonathan8@gmail.com>
2026-03-30 08:06:07 -0700

    Item system done

--- Assets/Jonathan Folder/Scripts/CurseRecipe.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CurseRecipe", menuName = "Scriptable Objects/CurseRecipe")]
-public class CurseRecipe : ScriptableObject
-{
-    public Curse[] curseingredients;
-    public Curse output;
-}
